fcc226dba1758ca3c25a72a8881d315ee0a3e1e9
J. Wilton <71266742+Wilton16@users.noreply.github.com>
2021-12-14 22:29:36 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -3,21 +3,13 @@ import tweepy
 import json
 import os
 import sqlite3
-from spotifyapi import makeartistlist, rapcaviar, pophits, country
-#from tweepy.client import Client
-
+from spotifyapi import artistlistfromplaylist, makeDatabase, rapcaviar, pophits, country
 
 authentication = tweepy.OAuthHandler("hg88yYv77DupNELwTvSuXjgWx", "LEvSK9ioZ0APvnYO5mYtHaHDSkaBFZfWT7O5s2r0MCpgKGGlN4", "twitter.com")
 authentication.set_access_token("2184902148-c9neyAw18DzbZbKU9XIvrTxk9sb74NrtCATHeCD", "8jEWIYQomGmt5uwO7Y8SSzg3CRv2yiKgzjPVNPPJuw93w")
 twitterapi = tweepy.API(auth = authentication)
 
-def makeDatabase(db_name):
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_name)
-    cur = conn.cursor()
-    return cur, conn
-
-def makeSpotifytable(cur, conn, info): 
+def makeTwittertable(cur, conn, info): 
     cur.execute('CREATE TABLE IF NOT EXISTS TwitterFollowers (Artist Text, Followers Integer)') 
     id = None
     cur.execute('SELECT max(Followers) FROM TwitterFollowers')
@@ -36,7 +28,7 @@ def makeSpotifytable(cur, conn, info):
         id = 0
     
     count = 0
-    while count < 25: #THIS SHOULD INSERT IT 25 AT A TIME!?
+    while count < 25:
         cur.execute("INSERT OR IGNORE INTO TwitterFollowers (Artist, Followers) VALUES (?, ?)", info)
         conn.commit()
         id += 1
@@ -48,15 +40,10 @@ def artistsearchtwitter(artistname):
     return topresult
 
 def spotifytwitterlookup(playlistids):
-    #artistfollowers=[]
     followerdict = {}
     for id in playlistids:
-        for artist in makeartistlist([id]):
-            #followerdict = {}
+        for artist in artistlistfromplaylist([id]):
             followerdict[artist] = artistsearchtwitter(artist)
-            #artistfollowers.append(followerdict)
-            #artistfollowers.append(artist + " Followers: " + str(artistsearchtwitter(artist)))
-    #return artistfollowers
     return followerdict
 
-print(spotifytwitterlookup([rapcaviar, pophits, country]))
+#print(spotifytwitterlookup([rapcaviar, pophits, country]))
