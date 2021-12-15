70d1b8632b1531f04863302ba4715350f05cc75a
J. Wilton <71266742+Wilton16@users.noreply.github.com>
2021-12-15 11:35:54 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -9,44 +9,51 @@ authentication = tweepy.OAuthHandler("hg88yYv77DupNELwTvSuXjgWx", "LEvSK9ioZ0APv
 authentication.set_access_token("2184902148-c9neyAw18DzbZbKU9XIvrTxk9sb74NrtCATHeCD", "8jEWIYQomGmt5uwO7Y8SSzg3CRv2yiKgzjPVNPPJuw93w")
 twitterapi = tweepy.API(auth = authentication)
 
-def makeTwittertable(cur, conn, info): 
+def makeTwittertable(cur, conn): 
     """Creates a Table in the Database for Twitter"""
+    cur.execute('DROP TABLE IF EXISTS SpotifyArtist')
     cur.execute('CREATE TABLE IF NOT EXISTS TwitterFollowers (Artist Text, Followers Integer)') 
-    id = None
-    cur.execute('SELECT max(Followers) FROM TwitterFollowers')
-    try:
-        row = cur.fetchone()
-
-        if row is None:
-            id = 0
-
-        else:
-            id = row[0]
-    except:
-        id = 0
-
-    if id is None:
-        id = 0
-    
-    count = 0
-    while count < 25:
-        cur.execute("INSERT OR IGNORE INTO TwitterFollowers (Artist, Followers) VALUES (?, ?)", info)
-        conn.commit()
-        id += 1
-        count += 1
     conn.commit()
+
+def inserttwitterdata(cur, conn, info):
+    '''Inserts Twitter Data into Table'''
+    cur.execute('INSERT INTO TwitterFollowers (Artist, Followers) VALUES (?,?)', info)
+    conn.commit()
+
 
 def artistsearchtwitter(artistname):
     "Searches for an artist on twitter and pulls their follower count"
     topresult = twitterapi.search_users(q=artistname)[0]._json["followers_count"]
     return topresult
 
-def spotifytwitterlookup(playlistids):
+def spotifytwitterlookup(playlistids, followerdict = {}):
     """Takes a list from Spotify of artists that appear on a playlist and Searches for individual artists on twitter"""
-    followerdict = {}
-    for id in playlistids:
-        for artist in artistlistfromplaylist([id]):
+    for id in [playlistids]:
+        for artist in artistlistfromplaylist(id):
             followerdict[artist] = artistsearchtwitter(artist)
     return followerdict
 
 #print(spotifytwitterlookup([rapcaviar, pophits, country]))
+
+def main():
+    playlists = {"rapcaviar": '37i9dQZF1DX0XUsuxWHRQd', "pophits" : '37i9dQZF1DXcBWIGoYBM5M', "country": '37i9dQZF1DX1lVhptIYRda'}
+    followers= {}
+    for playlist in playlists.values():
+        #print(playlist)
+        for genre in [spotifytwitterlookup(playlist, followers)]:
+            continue
+    conn = sqlite3.connect('spotifyartists.db')
+    cur = conn.cursor()
+    makeTwittertable(cur, conn)
+    items = str(followers.items()).strip("dict_items(")[:-1].strip('[(').strip(')]').split('), (')
+    tup = []
+    for item in items:
+        item = item.split(', ')
+        item = tuple(item)
+        tup.append(item)
+    for tupl in tup:        
+        inserttwitterdata(cur, conn, tupl)
+    
+
+if __name__ == "__main__":
+    main()
